894f58d4c70ac1d6dc9753c44678641cc52a585a
Nabhan Suwanachote <nabhan.au@gmail.com>
2021-11-28 21:05:05 +0700

    add chrome option and user

--- link_scan.py
+++ link_scan.py
@@ -1,8 +1,10 @@
 import os
 import sys
 import urllib
+import urllib.request
 from typing import List
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def get_links(url: str):
     """Find all links on page at the given url.
@@ -11,16 +13,22 @@ def get_links(url: str):
         a list of all unique hyperlinks on the page,
         without page fragments or query parameters.
     """
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
     no_dup_link = []
-    selenium = webdriver.Chrome()
-    selenium.get(url)
-    links = selenium.find_elements_by_xpath("//a[@href]")
-    for i in links:
-        link = i.get_attribute("href")
-        link = link.split("#")
-        link = link[0].split("?")
-        if link[0] not in no_dup_link and link[0] != '':
-            no_dup_link.append(link[0])
+    if url != []:
+        selenium = webdriver.Chrome(options=options)
+        selenium.get(url)
+        links = selenium.find_elements_by_xpath("//a[@href]")
+        for i in links:
+            link = i.get_attribute("href")
+            link = link.split("#")
+            link = link[0].split("?")
+            if link[0] not in no_dup_link and link[0] != '':
+                no_dup_link.append(link[0])
     return no_dup_link
     
     
@@ -33,8 +41,11 @@ def is_valid_url(url: str)->bool:
     Returns:
         bool: retrun true if url is valid
     """
+    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+    headers={'User-Agent':user_agent,}
+    request=urllib.request.Request(url,None,headers)
     try:
-        urllib.request.urlopen(url)
+        urllib.request.urlopen(request)
         return True
     except urllib.error.HTTPError:
         return False
@@ -59,7 +70,11 @@ def invalid_urls(urllist: List)->List:
 # get_links("https://cpske.github.io/ISP/")
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    try:
+        url = sys.argv[1]
+    except:
+        print("Usage:  python3 link_scan.py url \n\nTest all hyperlinks on the given url.")
+        url = []
     all_urls = get_links(url)
     bad_urls = invalid_urls(all_urls)
     print_str = ""
